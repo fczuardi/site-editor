9379160d3e6611691845c3ffc74e2c62951cd6ca
Fabricio Zuardi <fabricio@Zuardi-main.local>
2008-05-20 21:38:39 -0700

    some vintage networks (Ning v1) doesnt have the XN_Request::signInUrl and XN_Request::signOutUrl, using a hardcoded url when that is not available

--- src/init.php
+++ src/init.php
@@ -1,10 +1,12 @@
 <?php
+$currentUrl     = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $isLogged       = XN_Profile::current()->isLoggedIn();
 $app            = XN_Application::load();
 $appSubdomain   = $app->relativeUrl;
 $appUrl         = 'http://' . XN_AtomHelper::HOST_APP($appSubdomain);
 $isOwner        = XN_Profile::current()->isOwner();
-$signInUrl      = XN_Request::signInUrl();
-$signOutUrl     = XN_Request::signOutUrl();
+//some old Ning networks don't have the XN_Request::signInUrl /Out methods
+$signInUrl      = method_exists('XN_Request', 'signInUrl') ? XN_Request::signInUrl() : 'http://www.ning.com/main/authorization/signIn?target=' . urlencode($currentUrl);
+$signOutUrl     = method_exists('XN_Request', 'signOutUrl') ? XN_Request::signOutUrl() : 'http://www.ning.com/main/authorization/signOut?target=' . urlencode($currentUrl);
 //$viewSourceUrl  = "http://www.ning.com/view-source.html?appUrl=$appSubdomain";
 ?>
